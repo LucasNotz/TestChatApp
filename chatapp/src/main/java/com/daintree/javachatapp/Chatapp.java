ab34c1ba6040dfc6a6ba06e93350daf8f4dc623d
debiancomputer <alliedmastercomputer@debian.computer>
2025-10-02 23:26:20 +0000

    setting up project

---
+++ chatapp/src/main/java/com/daintree/javachatapp/Chatapp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.daintree.javachatapp;
+
+/**
+ *
+ * @author alliedmastercomputer
+ */
+public class Chatapp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
